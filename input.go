7746230f30f8e64a6fd7bf680e71542581dee992
web <liuyangc3@126.com>
2016-01-21 10:22:38 +0800

    Create input.go

---
+++ input.go
@@ -0,0 +1,78 @@
+package heka_redis_input
+
+import (
+	"fmt"
+	"time"
+	"github.com/garyburd/redigo/redis"
+	"github.com/mozilla-services/heka/pipeline"
+)
+
+type RedisListInputConfig struct {
+	Address  string `toml:"address"`
+	ListName string `toml:"key"`
+}
+
+type RedisListInput struct {
+	conf *RedisListInputConfig
+	conn redis.Conn
+}
+
+func (rli *RedisListInput) ConfigStruct() interface{} {
+	return &RedisListInputConfig{"localhost:6379", "heka"}
+}
+
+func (rli *RedisListInput) Init(config interface{}) error {
+	rli.conf = config.(*RedisListInputConfig)
+	var err error
+	rli.conn, err = redis.Dial("tcp", rli.conf.Address)
+	if err != nil {
+		return fmt.Errorf("connecting to - %s", err.Error())
+	}
+	return nil
+}
+
+func (rli *RedisListInput) Run(ir pipeline.InputRunner, h pipeline.PluginHelper) error {
+	var (
+		pack    *pipeline.PipelinePack
+		packs []*pipeline.PipelinePack
+	)
+
+	// Get the InputRunner's chan to receive empty PipelinePacks
+	inChan := ir.InChan()
+
+	for {
+		message, err := rli.conn.Do("RPOP", rli.conf.ListName)
+		if err != nil {
+			ir.LogError(fmt.Errorf("Redis RPOP error: %s", err))
+			// TODO: should reconnect redis rather than colse it
+			rli.Stop()
+			break
+		}
+		if message != nil {
+			pack = <-inChan
+			pack.Message.SetType("redis_list")
+			pack.Message.SetPayload(string(message.([]uint8)))
+			packs = []*pipeline.PipelinePack{pack}
+			if packs != nil {
+				for _, p := range packs {
+					ir.Inject(p)
+				}
+			} else {
+				pack.Recycle(nil)
+			}
+		} else {
+			time.Sleep(time.Second)
+		}
+	}
+	return nil
+}
+
+func (rli *RedisListInput) Stop() {
+	rli.conn.Close()
+}
+
+func init() {
+	pipeline.RegisterPlugin("RedisListInput", func() interface{} {
+		return new(RedisListInput)
+	})
+}
